30e6e579333a498991865ca29241dbbd89a045cd
Maksim Bashchuk <Maksim_Bashchuk@epam.com>
2023-04-13 04:05:05 +0300

    fix url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   products:
-    "https://curgd5sbhd.execute-api.us-east-1.amazonaws.com/dev/products",
+    "https://t181oeitnb.execute-api.us-east-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
